72041a5d20f827526761b27a1f81eb0979f42d34
Mohammad Najafian <loverdeveloper@outlook.com>
2020-09-25 00:52:37 +0330

    init

---
+++ content.js
@@ -0,0 +1,11 @@
+Array(...document.getElementsByClassName('cost')).map(element => {
+    element.style.filter = 'none';
+});
+
+Array(...document.getElementsByClassName('blured')).map(element => {
+    element.style.filter = 'none';
+});
+
+Array(...document.getElementsByClassName('havaaleh-blured-detail')).map(element => {
+    element.style.filter = 'none';
+});
